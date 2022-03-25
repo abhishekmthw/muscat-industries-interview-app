ce56a579545bdf28cd1521fb8d95bf0c295e557e
abhishek.mathew <abhishek.mathew@focaloid.com>
2022-03-25 10:16:13 +0530

    added mongo url to .env

--- server.js
+++ server.js
@@ -3,14 +3,13 @@ const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
+require("dotenv").config();
+
 const router = require("./router");
 
 app.use(bodyParser.json());
 app.use("/user", router);
 
-mongoose.connect(
-  "mongodb+srv://user:admin1234@cluster0.jj26t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  () => console.log("Connected to DB")
-);
+mongoose.connect(process.env.DB_CONNECT, () => console.log("Connected to DB"));
 
 app.listen(4000, () => console.log("Server Started!"));
